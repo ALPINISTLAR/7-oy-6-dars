1d629ac7e06bb84886f65d1059ab18d41c36eb2d
ALPINISTLAR <niveam424@gmail.com>
2024-02-21 07:17:11 +0200

    add components

---
+++ src/views/components/Background.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Background() {
+  return (
+    <div className="fixed top-0 left-0 overflow-hidden inset-0 flex justify-center items-center gap-20 background_square -z-10">
+    </div>
+  );
+}
