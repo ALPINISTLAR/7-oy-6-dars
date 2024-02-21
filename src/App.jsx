13542ced0a950803fce3d03ac9a7e467fb4d8d65
ALPINISTLAR <niveam424@gmail.com>
2024-02-21 07:14:57 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Layout from "./views/layout/Layout";
+import Request from "./views/REQUEST/Request";
+import Response from "./views/RESPONSE/Response";
+import { getFromHistory } from "./utils/localStorage";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [initialRequest, setInitialRequest] = useState(null)
+  const initialResponse = { data : "{\n\t\n}", atFirst : true}
+  const [data, setData] = useState(initialResponse);
+  const [sending, setSending] = useState(false);
+  const [isHistoryOpen, setIsHistoryOpen] = useState(false)
+  const [history, setHistory] = useState(getFromHistory())
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout openHistory={setIsHistoryOpen}>
+        <Request setResponse={setData} sendingState={[sending, setSending]} setHistory={setHistory} initialRequest={initialRequest} />
+        <Response response={data} />
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
